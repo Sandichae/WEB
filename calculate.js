313a0a098a8c0376df414bef5ea2b4965f052ae7
Sandichae <162092298+Sandichae@users.noreply.github.com>
2024-03-04 12:56:38 +0630

    Calculator

---
+++ calculate.js
@@ -0,0 +1,12 @@
+let num1 = document.getElementById("firstNumber");
+let num2 = d0cument.getElementById("secNumber");
+let operator = document.getElementById("operations");
+let btn = document.getElementById("btnCalculate");
+let result = document.getElementById("result")
+
+
+btn.addEventListener("click",function(){
+     console.log(operator.value);
+
+})
+
